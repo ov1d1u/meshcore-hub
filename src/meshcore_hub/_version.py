40dc6647e976812d68e3bbca662f6812ff6f6ca6
Louis King <jinglemansweep@gmail.com>
2025-12-07 22:02:42 +0000

    Updates

---
+++ src/meshcore_hub/_version.py
@@ -0,0 +1,8 @@
+"""MeshCore Hub version information.
+
+This file contains the version string for the package.
+It can be overridden at build time by setting BUILD_VERSION environment variable.
+"""
+
+__version__ = "dev"
+__all__ = ["__version__"]
